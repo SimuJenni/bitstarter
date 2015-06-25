6f5d9960a6fd89b6402605a407faecd973b2a4e9
Simon Jenni <simujenni@gmail.com>
2015-06-25 22:34:21 +0000

    Reading from index.html now

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var app = express();
+var fs = require('fs')
+var buffer = new Buffer(fs.readFileSync("index.html","utf-8"))
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!')
+  response.send(buffer.toString("utf-8"))
 })
 
 app.listen(app.get('port'), function() {
