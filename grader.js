1774d00c08a42c274b3c3988356db057e21bd321
Simon Jenni <simujenni@gmail.com>
2015-06-26 14:27:56 +0000

    started implementing commandline url args

--- grader.js
+++ grader.js
@@ -26,6 +26,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,10 +63,15 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var getHtml = function(fn) {
+    rest.get(fn).on('success', clone(data))
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <html_file>', 'URL to index.html', clone(assertFileExists))
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
