6ee218535774a085484d47aef45e0a1dfe7b7985
Simon Jenni <simujenni@gmail.com>
2015-06-27 14:49:25 +0000

    edited index.html

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var fs = require('fs')
 var buffer = new Buffer(fs.readFileSync("index.html","utf-8"))
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
